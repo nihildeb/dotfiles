a8106d22027c6c614162746de6a5151f828f0034
Nick Hildebrant <nick.hildebrant@gmail.com>
2013-09-08 16:18:31 +0200

    just keep bash it

--- bash/bash.symlink/completions.sh
+++
@@ -1,4 +0,0 @@
-source ~/.bash/completion_scripts/git_completion
-complete -o default -o nospace -F _git_branch gb
-complete -o default -o nospace -F _git_checkout gco
-complete -cf sudo
--- bash/bash.symlink/paths.sh
+++
@@ -1,3 +0,0 @@
-export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/git/bin:$PATH"
-export PATH="./bin:$HOME/.bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
