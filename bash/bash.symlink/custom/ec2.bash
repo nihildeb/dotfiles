696a8eea6dfe07de7ebc392f4dc22c641cc5ae6d
Nick Hildebrant <nick.hildebrant@gmail.com>
2014-05-20 00:13:43 +0200

    update

---
+++ bash/bash.symlink/custom/ec2.bash
@@ -0,0 +1,13 @@
+# for ubuntu
+alias ec2='ssh -i ~/.ssh/nihildeb.pem ubuntu@ec2-54-83-38-16.compute-1.amazonaws.com'
+alias lc2='ssh -i ~/.ssh/nihildeb.pem ubuntu@10.0.2.15'
+#alias ec2www='ssh -i ~/.ssh/nihildeb.pem dokku@ec2-54-83-38-16.compute-1.amazonaws.com'
+#alias ec2dokku='ssh -i ~/.ssh/id_rsa dokku@ec2-54-83-38-16.compute-1.amazonaws.com'
+
+# for amazon std
+#alias ec2='ssh -i ~/.ssh/nihildeb.pem ec2-user@ec2-54-83-38-16.compute-1.amazonaws.com'
+#alias ec2www='ssh -i ~/.ssh/id_rsa nihildeb@ec2-54-83-38-16.compute-1.amazonaws.com'
+
+
+#cat ~/.ssh/id_rsa.pub | ssh -i ~/.ssh/nihildeb.pem dokku@ec2-54-83-38-16.compute-1.amazonaws.com "sudo sshcommand acl-add dokku nihildeb"
+
