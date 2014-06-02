dad1031c6da1682f1b0567c700320d885df18bf4
Nick Hildebrant <nick.hildebrant@gmail.com>
2014-06-02 15:04:57 +0200

    install docker on ubuntu

--- bash/bash.symlink/ubuntu.sh
+++ bash/bash.symlink/ubuntu.sh
@@ -3,9 +3,15 @@
 ubup() {
   apt-get update
   apt-get install -y git gcc make curl wget nc dnsutils
+}
+
+ubdocker_install() {
   wget https://get.docker.io/builds/Linux/x86_64/docker-latest -O docker
   chmod +x docker
   mv docker /usr/local/bin
+  cd /
+  tar xvf ~/.dotfiles/docker/docker_init.tar
+  service docker start
 }
 
 
