a0bb6351ce6d2fe7e41f0ceb0c6fa52c9cfae541
Nick Hildebrant <nick.hildebrant@gmail.com>
2015-04-22 01:36:29 +0200

    updates for ec2 and vim chromeos fail

--- bash/bash.symlink/funs.sh
+++ bash/bash.symlink/funs.sh
@@ -13,7 +13,7 @@
 
 # vars
 RPI_IP_1='192.168.2.2'
-RPI_IP_2='192.168.2.3'
+STUDIO_IP='192.168.2.3'
 EC2_IP_1='54.86.18.40'
 EC2_IP_2='54.86.66.164'
 EC2_IP_TEMP='54.86.85.239'
@@ -29,11 +29,11 @@ this_file="$(dirname $BASH_SOURCE)/$(basename $BASH_SOURCE)"
 alias tmux='tmux attach || tmux'
 
 rpi() { ssh -Y pi@$RPI_IP_1 ;}
-pi() { ssh -Y $RPI_IP_2 ;}
+studio() { ssh -Y $STUDIO_IP ;}
 pis() { rsync -avz ~/sync/pd/ -e ssh $RPI_IP_2:/home/nihildeb/pd ;}
-ec2u() { ssh $EC2_PEM ubuntu@$EC2_IP_1 ;}
+ec2() { ssh $EC2_PEM ubuntu@$EC2_IP_1 ;}
 ec2t() { ssh $EC2_USER@$EC2_TESTHOST ;}
-ec2() { ssh $EC2_USER@$EC2_IP_1 ;}
+ec2old() { ssh $EC2_USER@$EC2_IP_1 ;}
 ec3() { ssh $EC2_PEM $EC2_USER@$EC2_IP_2 ;}
 ec4() { ssh $EC2_PEM $EC2_USER@$EC2_IP_TEMP ;}
 
